9ca9c67deb0c2547dca5d93234c74184121df91d
tao <taopopoo@126.com>
2014-07-04 15:24:14 +0800

    add getLocalIps function

--- common.go
+++ common.go
@@ -21,3 +21,28 @@ func GetLocalIntenetIp() string {
 	defer conn.Close()
 	return strings.Split(conn.LocalAddr().String(), ":")[0]
 }
+
+// This returns the list of local ip addresses which other hosts can connect
+// to (NOTE: Loopback ip is ignored).
+func GetLocalIPs() ([]*net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	ips := make([]*net.IP, 0)
+	for _, addr := range addrs {
+		ipnet, ok := addr.(*net.IPNet)
+		if !ok {
+			continue
+		}
+
+		if ipnet.IP.IsLoopback() {
+			continue
+		}
+
+		ips = append(ips, &ipnet.IP)
+	}
+
+	return ips, nil
+}
